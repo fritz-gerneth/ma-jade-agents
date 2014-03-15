99e061813e62da9e24ff2f872456d316d998fd88
Fritz Gerneth <fgerneth@effms.de>
2014-03-15 18:57:08 +0100

    Shorthand script to start Demo-MAS with finished agents

---
+++ startMAS.bat
@@ -0,0 +1,2 @@
+java jade.Boot -gui -jade_domain_df_autocleanup true -jade_core_management_AgentManagementService_agentspath ./target fakeGpsDevice:de.effms.jade.agent.gpsfakedevice.GpsFakeDevice
+pause
