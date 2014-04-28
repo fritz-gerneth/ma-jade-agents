946675c1b5aa65592c04430d3de22e45982552ac
Fritz Gerneth <fgerneth@effms.de>
2014-04-28 11:11:01 +0200

    Ontology to add user movement to User-Concept

---
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/UserMovementOntology.java
@@ -0,0 +1,31 @@
+package de.effms.marsdemo.ontology;
+
+import de.effms.jade.ontology.RecommenderSystemOntology;
+import jade.content.onto.BasicOntology;
+import jade.content.onto.Ontology;
+import jade.content.onto.OntologyException;
+import jade.content.onto.ReflectiveIntrospector;
+import jade.content.schema.ConceptSchema;
+import jade.content.schema.ObjectSchema;
+
+public class UserMovementOntology extends Ontology implements UserMovementVocabulary
+{
+    private static UserMovementOntology instance = new UserMovementOntology();
+
+    public static UserMovementOntology getInstance()
+    {
+        return instance;
+    }
+
+    private UserMovementOntology()
+    {
+        super(NAME, new Ontology[] {BasicOntology.getInstance(), RecommenderSystemOntology.getInstance(), CoordinateOntology.getInstance()}, new ReflectiveIntrospector());
+
+        try {
+            ConceptSchema userConcept = (ConceptSchema) getSchema(RecommenderSystemOntology.USER);
+            userConcept.add(HEADING_TOWARDS, (ConceptSchema) getSchema(CoordinateVocabulary.COORDINATE), ObjectSchema.OPTIONAL, ObjectSchema.UNLIMITED);
+        } catch (OntologyException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/UserMovementVocabulary.java
@@ -0,0 +1,8 @@
+package de.effms.marsdemo.ontology;
+
+public interface UserMovementVocabulary
+{
+    String NAME = "de.effms.marsdemo.ontology.UserMovement";
+
+    String HEADING_TOWARDS = "user-heading-towards";
+}
