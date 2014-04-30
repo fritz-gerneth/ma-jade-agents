3be3dff89ed6358dd40b40006ef94ae18e388b6a
Fritz Gerneth <fgerneth@effms.de>
2014-04-30 19:55:36 +0200

    Update overall project structure in pom.xml

--- startMAS.bat
+++ startMAS.bat
@@ -1,2 +1,2 @@
-java jade.Boot -gui -jade_domain_df_autocleanup true -jade_core_management_AgentManagementService_agentspath ./target fakeGpsDevice:de.effms.jade.agent.gpsfakedevice.GpsFakeDevice -agentlib:jdwp=transport=dt_socket,address=127.0.0.1:59093,suspend=y,server=n -Dfile.encoding=UTF-8
+java jade.Boot -gui -jade_domain_df_autocleanup true -jade_core_management_AgentManagementService_agentspath ./target fakeGpsDevice:de.effms.agent.ui.UserInterActionAgent -agentlib:jdwp=transport=dt_socket,address=127.0.0.1:59093,suspend=y,server=n -Dfile.encoding=UTF-8
 pause
