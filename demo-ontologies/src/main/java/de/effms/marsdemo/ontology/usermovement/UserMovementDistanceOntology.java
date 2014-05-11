5156324ac209c3051a3090dae917fc1492bf38f8
Fritz Gerneth <fgerneth@effms.de>
2014-05-11 14:37:17 +0200

    Add new ontology refining the HEADED_TO concept with a distance

---
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementDistanceOntology.java
@@ -0,0 +1,28 @@
+package de.effms.marsdemo.ontology.usermovement;
+
+import de.effms.marsdemo.ontology.coordinate.CoordinateOntology;
+import jade.content.onto.Ontology;
+import jade.content.onto.OntologyException;
+import jade.content.schema.ConceptSchema;
+
+public class UserMovementDistanceOntology extends Ontology implements UserMovementDistanceVocabulary
+{
+    private static UserMovementDistanceOntology instance = new UserMovementDistanceOntology();
+
+    public static UserMovementDistanceOntology getInstance()
+    {
+        return instance;
+    }
+
+    private UserMovementDistanceOntology()
+    {
+        super(NAME, CoordinateOntology.getInstance());
+
+        try {
+            final ConceptSchema headedToConcept = (ConceptSchema) getSchema(HEADED_TO);
+            headedToConcept.add(HEADED_TO_DISTANCE, (ConceptSchema) getSchema(DISTANCE));
+        } catch (OntologyException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementDistanceVocabulary.java
@@ -0,0 +1,8 @@
+package de.effms.marsdemo.ontology.usermovement;
+
+public interface UserMovementDistanceVocabulary extends UserMovementVocabulary
+{
+    public static String NAME = "de.effms.marsdemo.ontology.usermovement.UserMovementDistance";
+
+    public static String HEADED_TO_DISTANCE = "headed_to_distance";
+}
