fa98bfbb9f693227ad185bba7750d706d4822640
Fritz Gerneth <fgerneth@effms.de>
2014-05-11 12:40:38 +0200

    Add MOVES predicate

--- demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementOntology.java
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementOntology.java
@@ -1,9 +1,13 @@
 package de.effms.marsdemo.ontology.usermovement;
 
 import de.effms.marsdemo.ontology.coordinate.CoordinateOntology;
+import jade.content.abs.AbsObject;
 import jade.content.onto.Ontology;
 import jade.content.onto.OntologyException;
 import jade.content.schema.ConceptSchema;
+import jade.content.schema.Facet;
+import jade.content.schema.ObjectSchema;
+import jade.content.schema.PredicateSchema;
 
 public class UserMovementOntology extends Ontology implements UserMovementVocabulary
 {
@@ -19,11 +23,25 @@ public class UserMovementOntology extends Ontology implements UserMovementVocabu
         super(NAME, CoordinateOntology.getInstance());
 
         try {
-            ConceptSchema movesConcept = new ConceptSchema(MOVE);
+            final ConceptSchema movesConcept = new ConceptSchema(MOVE);
             movesConcept.addSuperSchema((ConceptSchema) getSchema(ACTION));
             movesConcept.add(MOVE_TO_POSITION, (ConceptSchema) getSchema(COORDINATE));
 
+            PredicateSchema movingPredicate = new PredicateSchema(MOVES);
+            movingPredicate.addSuperSchema((PredicateSchema) getSchema(DOES));
+
+            // Only allow the MOVE concept to be used as the subject of the MOVES predicate
+            movingPredicate.addFacet(DOES_WHAT, new Facet() {
+                public void validate(AbsObject value, Ontology onto) throws OntologyException {
+                    ObjectSchema valueSchema = onto.getSchema(value.getTypeName());
+                    if (!valueSchema.isCompatibleWith(movesConcept)) {
+                        throw new OntologyException("Value " + value + " is not a " + MOVE);
+                    }
+                }
+            });
+
             this.add(movesConcept);
+            this.add(movingPredicate);
         } catch (OntologyException e) {
             e.printStackTrace();
         }
--- demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementVocabulary.java
+++ demo-ontologies/src/main/java/de/effms/marsdemo/ontology/usermovement/UserMovementVocabulary.java
@@ -6,6 +6,8 @@ public interface UserMovementVocabulary extends CoordinateVocabulary
 {
     String NAME = "de.effms.marsdemo.ontology.usermovement.UserMovement";
 
+    String MOVES = "um_moves";
+
     String MOVE = "um_move";
 
     String MOVE_TO_POSITION = "um_move_to_pos";
