31f715513a2cd6381585b86ca396097fec0c8633
Fritz Gerneth <fgerneth@effms.de>
2014-05-12 12:37:47 +0200

    Use static import of vocabulary

--- UserInteractionAgent/src/main/java/de/effms/agent/ui/user/UserKnowledgeBase.java
+++ UserInteractionAgent/src/main/java/de/effms/agent/ui/user/UserKnowledgeBase.java
@@ -6,15 +6,13 @@ import de.effms.jade.service.publish.SubscriptionListener;
 import de.effms.jade.service.query.QueryIfCallback;
 import de.effms.jade.service.query.QueryRefCallback;
 import de.effms.jade.service.query.Queryable;
-import de.effms.marsdemo.ontology.car.CarInformationVocabulary;
-import de.effms.marsdemo.ontology.coordinate.CoordinateVocabulary;
 import de.effms.marsdemo.ontology.usermovement.UserMovementOntology;
-import de.effms.marsdemo.ontology.usermovement.UserMovementVocabulary;
 import jade.content.abs.*;
-import jade.content.lang.sl.SLVocabulary;
 import jade.content.onto.Ontology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static de.effms.marsdemo.ontology.usermovement.UserMovementDistanceVocabulary.*;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -23,8 +21,6 @@ import java.util.List;
 public class UserKnowledgeBase implements Queryable, Subscribable
 {
     private final Logger log = LoggerFactory.getLogger(UserKnowledgeBase.class);
-
-    private UserMovementOntology ontology = UserMovementOntology.getInstance();
 
     private HashMap<String, LinkedList<Subscription>> subscriptions = new HashMap<>();
 
@@ -36,31 +32,31 @@ public class UserKnowledgeBase implements Queryable, Subscribable
 
     public UserKnowledgeBase()
     {
-        this.headedToCoordinate = new AbsConcept(UserMovementVocabulary.COORDINATE);
+        this.headedToCoordinate = new AbsConcept(COORDINATE);
         this.setUserDestination(0, 0);
 
-        this.headedTo = new AbsConcept(UserMovementVocabulary.HEADED_TO);
-        this.headedTo.set(UserMovementVocabulary.HEADED_TO_POSITION, this.headedToCoordinate);
+        this.headedTo = new AbsConcept(HEADED_TO);
+        this.headedTo.set(HEADED_TO_POSITION, this.headedToCoordinate);
 
-        this.location = new AbsConcept(UserMovementVocabulary.COORDINATE);
+        this.location = new AbsConcept(COORDINATE);
         this.setUserLocation(0, 0);
     }
 
     public void setUserDestination(int x, int y)
     {
-        this.headedToCoordinate.set(UserMovementVocabulary.X, x);
-        this.headedToCoordinate.set(UserMovementVocabulary.Y, y);
+        this.headedToCoordinate.set(X, x);
+        this.headedToCoordinate.set(Y, y);
 
-        this.informSubscribers(UserMovementVocabulary.HEADED);
-        this.informSubscribers(UserMovementVocabulary.HEADED_TO_POSITION);
+        this.informSubscribers(HEADED);
+        this.informSubscribers(HEADED_TO_POSITION);
     }
 
     public void setUserLocation(int x, int y)
     {
-        this.location.set(UserMovementVocabulary.X, x);
-        this.location.set(UserMovementVocabulary.Y, y);
+        this.location.set(X, x);
+        this.location.set(Y, y);
 
-        this.informSubscribers(UserMovementVocabulary.IS_LOCATED);
+        this.informSubscribers(IS_LOCATED);
     }
 
     public AbsConcept getUserDestination()
@@ -85,12 +81,9 @@ public class UserKnowledgeBase implements Queryable, Subscribable
         callback.onQueryRefResult(query, this.answer(query));
     }
 
-    private AbsPredicate answer(AbsIRE query)
+    private AbsConcept answer(AbsIRE query)
     {
         String propositionTypeName = query.getProposition().getTypeName();
-        AbsPredicate answer = new AbsPredicate(SLVocabulary.EQUALS);
-        answer.set(SLVocabulary.EQUALS_LEFT, query);
-
         /**
          * Answering the HEADED predicate
          *
@@ -108,13 +101,13 @@ public class UserKnowledgeBase implements Queryable, Subscribable
          * we don't have to check which one contains the variable and which ones are set as additional constraints.
          * We don't check if all variables are set.
          */
-        if (propositionTypeName.equals(UserMovementVocabulary.HEADED)) {
+        if (propositionTypeName.equals(HEADED)) {
             AbsPredicate headed = query.getProposition();
-            AbsObject what = headed.getAbsObject(UserMovementVocabulary.IS_WHAT);
+            AbsObject what = headed.getAbsObject(IS_WHAT);
             if (what instanceof AbsVariable) {
-                answer.set(SLVocabulary.EQUALS_RIGHT, this.headedTo);
+                return this.headedTo;
             } else if (what instanceof AbsConcept) {
-                answer.set(SLVocabulary.EQUALS_RIGHT, this.headedToCoordinate);
+                return this.headedToCoordinate;
             }
         }
         /**
@@ -125,33 +118,31 @@ public class UserKnowledgeBase implements Queryable, Subscribable
          *
          * The same limitations as for HEADED apply
          */
-        else if (propositionTypeName.equals(UserMovementVocabulary.IS_LOCATED)) {
+        else if (propositionTypeName.equals(IS_LOCATED)) {
             AbsPredicate isLocated = query.getProposition();
-            AbsObject isWhat = isLocated.getAbsObject(UserMovementVocabulary.IS_WHAT);
+            AbsObject isWhat = isLocated.getAbsObject(IS_WHAT);
             if (isWhat instanceof AbsVariable) {
-                answer.set(SLVocabulary.EQUALS_RIGHT, this.location);
+                return this.location;
             }
             // as IS_WHO is the only other slot, its either this (not supported) or no variable at all (not supported)
         }
 
-        return answer;
+        return null;
     }
 
     @Override
     public Subscription subscribe(AbsIRE query)
     {
         Subscription subscription = new Subscription(query);
-        String propositionTypeName = query.getProposition().getTypeName();
-
-        if (!this.subscriptions.containsKey(UserMovementVocabulary.HEADED_TO_POSITION)) {
-            this.subscriptions.put(UserMovementVocabulary.HEADED_TO_POSITION, new LinkedList<Subscription>());
+        if (!this.subscriptions.containsKey(HEADED_TO_POSITION)) {
+            this.subscriptions.put(HEADED_TO_POSITION, new LinkedList<Subscription>());
         }
 
-        this.subscriptions.get(UserMovementVocabulary.HEADED_TO_POSITION).add(subscription);
+        this.subscriptions.get(HEADED_TO_POSITION).add(subscription);
 
         SubscriptionListener callback = subscription.getCallback();
         if (null != callback) {
-            callback.onInform(this.answer(query));
+            callback.onInform(query, this.answer(query));
         }
 
         return subscription;
@@ -172,7 +163,7 @@ public class UserKnowledgeBase implements Queryable, Subscribable
             SubscriptionListener callback = s.getCallback();
             if (null != callback) {
                 log.info("Informing subscriber with query " + s.getQuery());
-                callback.onInform(this.answer(s.getQuery()));
+                callback.onInform(s.getQuery(), this.answer(s.getQuery()));
             }
         }
     }
@@ -186,6 +177,6 @@ public class UserKnowledgeBase implements Queryable, Subscribable
     @Override
     public Ontology getOntology()
     {
-        return ontology;
+        return UserMovementOntology.getInstance();
     }
 }
